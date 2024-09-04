a945bb3500b24db1be2e2dd1b099359803af73d2
Arthurchkeee <Arthurchkeee@gmail.com>
2024-09-04 17:12:37 +0300

    Init: start Traintix project

---
+++ src/main/java/com/traintix/TraintixApplication.java
@@ -0,0 +1,13 @@
+package com.traintix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraintixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraintixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/traintix/TraintixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traintix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraintixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
